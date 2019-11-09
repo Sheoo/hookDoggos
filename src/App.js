33e8c5243346516aa0813ed4c843defb4662c320
Robin Guerre <rb.guerre@gmail.com>
2019-11-09 22:53:58 +0100

    DOGGOS !

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      breeds: [],
+      selection: ""
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://dog.ceo/api/breeds/list/all")
+      .then(res => res.json())
+      .then(data => this.setState({ breeds: Object.keys(data.message) }));
+  }
+
+  handleSelection = breed =>
+    fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+      .then(res => res.json())
+      .then(data => this.setState({ selection: data.message }));
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div style={{ height: "500px" }}>
+            {this.state.selection ? (
+              <img
+                src={this.state.selection}
+                alt=""
+                style={{ width: "auto", height: "100%" }}
+              />
+            ) : (
+              <p>Choose a dog</p>
+            )}
+          </div>
+          <div style={{ display: "inline" }}>
+            {this.state.breeds.map((element, i) => (
+              <button key={i} onClick={() => this.handleSelection(element)}>
+                {element}
+              </button>
+            ))}
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
