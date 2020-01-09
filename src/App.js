6db8703329327169d0f8ede8f28788349428a23e
Robin Guerre <rb.guerre@gmail.com>
2020-01-09 09:14:39 +0100

    create parent component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,15 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
+import React, { useState } from "react";
+import DoggoDoggo from "./DoggoDoggo";
 
 const App = () => {
-  const [breeds, setBreeds] = useState([]);
-  const [selection, setSelection] = useState("");
-
-  useEffect(() => {
-    fetch("https://dog.ceo/api/breeds/list/all")
-      .then(res => res.json())
-      .then(data => setBreeds(Object.keys(data.message)));
-  });
-
-  const handleSelection = breed =>
-    fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
-      .then(res => res.json())
-      .then(data => setSelection(data.message));
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <div style={{ height: "500px" }}>
-          {selection ? (
-            <img
-              src={selection}
-              alt=""
-              style={{ width: "auto", height: "100%" }}
-            />
-          ) : (
-            <p>Choose a dog</p>
-          )}
-        </div>
-        {React.Children.map(breeds, element => (
-          <button style={{ display: "inline" }} onClick={() => handleSelection(element)}>{element}</button>
-        ))}
-      </header>
+    <div>
+      <DoggoDoggo
+        count={count}
+        setCount={setCount}
+      />
     </div>
   );
 };
